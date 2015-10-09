29461c243d259dfd93412dbd732564ac4e850c1b
Bernardo <berzuca@msn.com>
2015-10-08 23:27:53 -0200

    adding animate.css, love.bg, basic template

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -2,7 +2,8 @@
         app.controller('HomeController', HomeController);
         HomeController.$inject = ["$scope"];
         function HomeController($scope) {
-
+            $scope.todayDate = new Date();
+            $scope.todayMessage = "Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.";
 
         }
 })();
